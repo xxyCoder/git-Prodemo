fc4ace8fe83d57ffa798874026be8a2e573d3d3f
xxy <1696944145@qq.com>
2022-07-24 22:04:42 +0800

    xxy commit and update Demo1

--- src/main/java/com/xxy/git/Demo1.java
+++ src/main/java/com/xxy/git/Demo1.java
@@ -3,5 +3,6 @@ package com.xxy.git;
 public class Demo1 {
     public static void main(String[] args) {
         System.out.println("hell git");
+        System.out.println("the modify1");
     }
 }
